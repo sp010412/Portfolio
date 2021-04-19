4be51e71a12519141fed1955cc0367e3e16c2f0b
sp010412 <smajwede7@gmail.com>
2021-04-19 16:47:21 +0200

    restucture

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ $(document).ready(function () {
             $('.navbar').removeClass("sticky");
         }
     })
+
     // toggle menu/navbar script
     $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass('.active')
         $('.menu-btn i').toggleClass('.active')
     });
 });
+
+//owl carousel script
+$('.')
