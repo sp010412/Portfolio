7201c093f5a0b0d15bce8ee92aa90b318cbcf2e7
sp010412 <smajwede7@gmail.com>
2021-05-14 15:55:34 +0200

    complete

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ $(document).ready(function () {
         }
     })
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function () {
-        $('.navbar .menu').toggleClass('.active')
-        $('.menu-btn i').toggleClass('.active')
-    });
 });
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
